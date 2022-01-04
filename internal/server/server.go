3e3b6bd8c1a450776b68c141a26ea02f0419e77b
2thousandmax <2thousandmax@gmail.com>
2022-01-04 17:41:00 +0300

    Implement http server

---
+++ internal/server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"context"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    ":8080",
+			Handler: handler,
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
