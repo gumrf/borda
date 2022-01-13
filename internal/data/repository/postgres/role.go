abfa6acb80c3926557be01a26d9eb2d8e94eee8f
rmnvlv <romario2000@bk.ru>
2022-01-13 21:12:57 +0300

    Update role-postgr-repo

--- internal/data/repository/postgres/role.go
+++ internal/data/repository/postgres/role.go
@@ -16,18 +16,27 @@ func NewPostrgresRoleRepository(db *sqlx.DB) *PostrgresRoleRepository {
 	return &PostrgresRoleRepository{db: db}
 }
 
-func (role *PostrgresRoleRepository) Get(id int) (int, error) {
+func (role *PostrgresRoleRepository) Get(id int) (string, error) {
 	qwery := "SELECT role_id FROM user_roles WHERE user_Id = $1"
 
 	var a int
 	err := role.db.QueryRowx(qwery, id).Scan(&a)
 	if err != nil {
-		return -1, err
+		return "", err
 	}
-	return a, nil
+
+	qwery = "SELECT role_name FROM role WHERE Id = $1"
+
+	var b string
+	err = role.db.QueryRowx(qwery, a).Scan(&b)
+	if err != nil {
+		return "", err
+	}
+
+	return b, nil
 }
 
-func (role *PostrgresRoleRepository) GiveRole(userId, roleId int) error {
+func (role *PostrgresRoleRepository) Give(userId, roleId int) error {
 	qwery := "INSERT INTO user_roles (user_id, role_id) VALUES(?, ?)"
 
 	_, err := role.db.Exec(qwery, userId, roleId)
