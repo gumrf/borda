c04e10d45c1e19c0da084918100268d2d00e7394
rmnvlv <romario2000@bk.ru>
2022-03-23 21:55:35 +0300

    add: errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -2,8 +2,12 @@ package domain
 
 import "errors"
 
-var ErrUserAlreadyExists = errors.New("user already exists")
+var ErrUserAlreadyExists = errors.New("User already exists")
 
-var ErrInvalidInput = errors.New("invalid input")
+var ErrInvalidInput = errors.New("Invalid input")
 
 var ErrInvalidTeamInput = errors.New("ErrInvalidTeamInput")
+
+var ErrTeamAlreadyExists = errors.New("Team already exists")
+
+var ErrTeamTokenIsInvalid = errors.New("Team token is invalid or doesent exists")
