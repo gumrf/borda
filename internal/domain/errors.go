d5f66a9d241b2d43085d47c87c4b519515089743
rmnvlv <romario2000@bk.ru>
2022-04-06 01:13:10 +0300

    add arrors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -11,3 +11,11 @@ var ErrInvalidTeamInput = errors.New("errInvalidTeamInput")
 var ErrTeamAlreadyExists = errors.New("team already exists")
 
 var ErrTeamTokenIsInvalid = errors.New("team token is invalid or doesent exists")
+
+var ErrUserNotFound = errors.New("user not found")
+
+var ErrUsersNotFound = errors.New("users not found")
+
+var ErrTeamNotFound = errors.New("team not found")
+
+var ErrMembers = errors.New("members not found")
