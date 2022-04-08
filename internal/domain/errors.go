fc18f4ef33c3a51cd8ea32f0d07056df70218fda
rmnvlv <romario2000@bk.ru>
2022-04-08 17:45:26 +0300

    add errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -11,3 +11,11 @@ var ErrInvalidTeamInput = errors.New("errInvalidTeamInput")
 var ErrTeamAlreadyExists = errors.New("team already exists")
 
 var ErrTeamTokenIsInvalid = errors.New("team token is invalid or doesent exists")
+
+var ErrTaskCreate = errors.New("can't create new task")
+
+var ErrTaskUpdate = errors.New("can't update task")
+
+var ErrTaskNotFound = errors.New("task not found")
+
+var ErrTasksNotFound = errors.New("tasks not found")
