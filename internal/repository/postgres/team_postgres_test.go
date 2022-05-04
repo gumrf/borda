70401256b7248026403ba71ed3611f28b7f1f789
rmnvlv <romario2000@bk.ru>
2022-05-04 23:20:49 +0300

    fix: team tests

--- internal/repository/postgres/team_postgres_test.go
+++ internal/repository/postgres/team_postgres_test.go
@@ -5,18 +5,23 @@ import (
 	"borda/internal/repository/postgres"
 	"testing"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTeamRepository_SaveTeam(t *testing.T) {
 	db := MustOpenDB(t)
-	userRepo := postgres.NewUserRepository(db)
+	defer MustCloseDB(t, db)
+
 	teamRepo := postgres.NewTeamRepository(db)
 	requre := require.New(t)
 
 	type args struct {
 		teamLeaderId int
 		teamName     string
+		username     string
+		password     string
+		contact      string
 	}
 	testTable := []struct {
 		name         string
@@ -28,17 +33,25 @@ func TestTeamRepository_SaveTeam(t *testing.T) {
 		{
 			name: "OK",
 			args: args{
-				teamLeaderId: 4,
+				teamLeaderId: 1,
 				teamName:     "Momstr",
+				username:     "TestUser",
+				password:     "TestPswd",
+				contact:      "@testcontact",
 			},
-			wantResponse: 4,
+			wantResponse: 1,
 			wantErr:      nil,
 		},
 	}
 	for _, testCase := range testTable {
 		t.Run(testCase.name, func(t *testing.T) {
-			_, err := userRepo.SaveUser("TestUser", "TestPswd", "@testcontact")
-			requre.Equal(testCase.wantErr, err, t)
+			user := &domain.User{
+				Username: testCase.args.username,
+				Password: testCase.args.password,
+				Contact:  testCase.args.contact,
+			}
+
+			helpCreateUser(t, db, user)
 
 			actualResponse, actualErr := teamRepo.SaveTeam(testCase.args.teamLeaderId, testCase.args.teamName)
 
@@ -50,11 +63,16 @@ func TestTeamRepository_SaveTeam(t *testing.T) {
 
 func TestTeamRepository_GetTeamById(t *testing.T) {
 	db := MustOpenDB(t)
+	defer MustCloseDB(t, db)
+
 	teamRepo := postgres.NewTeamRepository(db)
 	requre := require.New(t)
 
 	type args struct {
-		teamId int
+		teamId   int
+		username string
+		password string
+		contact  string
 	}
 	testTable := []struct {
 		name         string
@@ -66,13 +84,15 @@ func TestTeamRepository_GetTeamById(t *testing.T) {
 		{
 			name: "OK",
 			args: args{
-				teamId: 1,
+				teamId:   1,
+				username: "User1",
+				password: "User1Pswd",
+				contact:  "@contact",
 			},
 			wantResponse: &domain.Team{
 				Id:           1,
 				Name:         "Team1",
 				TeamLeaderId: 1,
-				Token:        "7c8b4c73-9fdd-4fbb-b926-43de9aa6f24d",
 				Members: []domain.TeamMember{
 					{
 						UserId: 1,
@@ -85,33 +105,68 @@ func TestTeamRepository_GetTeamById(t *testing.T) {
 	}
 	for _, testCase := range testTable {
 		t.Run(testCase.name, func(t *testing.T) {
+			user := &domain.User{
+				Username: testCase.args.username,
+				Password: testCase.args.password,
+				Contact:  testCase.args.contact,
+			}
+			helpCreateUser(t, db, user)
+
+			team := &domain.Team{
+				Name:         testCase.wantResponse.Name,
+				TeamLeaderId: testCase.wantResponse.TeamLeaderId,
+			}
+			helpCreateTeam(t, db, team)
+
 			actualResponse, actualErr := teamRepo.GetTeamById(testCase.args.teamId)
 
 			requre.Equal(testCase.wantErr, actualErr, t)
-			requre.Equal(testCase.wantResponse, actualResponse, t)
+			requre.Equal(testCase.wantResponse.Id, actualResponse.Id, t)
+			requre.Equal(testCase.wantResponse.Members, actualResponse.Members, t)
+			requre.Equal(testCase.wantResponse.Name, actualResponse.Name, t)
+			requre.Equal(testCase.wantResponse.TeamLeaderId, actualResponse.TeamLeaderId, t)
 		})
 	}
 }
 
 func TestTeamRepository_GetTeams(t *testing.T) {
 	db := MustOpenDB(t)
+	defer MustCloseDB(t, db)
+
 	teamRepo := postgres.NewTeamRepository(db)
 	requre := require.New(t)
 
+	type args struct {
+		username string
+		password string
+		contact  string
+	}
 	testTable := []struct {
 		name         string
 		wantResponse []*domain.Team
 		wantErr      error
+		args         []args
 	}{
 		// TODO: Add test cases.
 		{
 			name: "OK",
+			args: []args{
+				{
+					username: "User1",
+					password: "User1Pswd",
+					contact:  "@contact1",
+				},
+				{
+					username: "User2",
+					password: "User2Pswd",
+					contact:  "@contact",
+				},
+			},
 			wantResponse: []*domain.Team{
 				{
 					Id:           1,
 					Name:         "Team1",
 					TeamLeaderId: 1,
-					Token:        "7c8b4c73-9fdd-4fbb-b926-43de9aa6f24d",
 					Members: []domain.TeamMember{
 						{
 							UserId: 1,
@@ -123,23 +178,10 @@ func TestTeamRepository_GetTeams(t *testing.T) {
 					Id:           2,
 					Name:         "Team2",
 					TeamLeaderId: 2,
-					Token:        "e01e949e-9dd0-428e-96c0-28adebf4df3d",
 					Members: []domain.TeamMember{
 						{
 							UserId: 2,
 							Name:   "User2",
-						},
-					},
-				},
-				{
-					Id:           3,
-					Name:         "Team3",
-					TeamLeaderId: 3,
-					Token:        "bd58e756-7ef3-4043-bf4c-2c5ae9b9ad0b",
-					Members: []domain.TeamMember{
-						{
-							UserId: 3,
-							Name:   "User3",
 						},
 					},
 				},
@@ -149,21 +191,47 @@ func TestTeamRepository_GetTeams(t *testing.T) {
 	}
 	for _, testCase := range testTable {
 		t.Run(testCase.name, func(t *testing.T) {
-			actualResponse, actualErr := teamRepo.GetTeams()
+			for _, arg := range testCase.args {
+				user := &domain.User{
+					Username: arg.username,
+					Password: arg.password,
+					Contact:  arg.contact,
+				}
 
+				helpCreateUser(t, db, user)
+			}
+
+			for _, team := range testCase.wantResponse {
+				helpCreateTeam(t, db, team)
+			}
+
+			actualResponse, actualErr := teamRepo.GetTeams()
 			requre.Equal(testCase.wantErr, actualErr, t)
-			requre.Equal(testCase.wantResponse, actualResponse, t)
+
+			for i, testTeam := range testCase.wantResponse {
+				requre.Equal(testTeam.Id, actualResponse[i].Id, t)
+				requre.Equal(testTeam.Name, actualResponse[i].Name, t)
+				requre.Equal(testTeam.TeamLeaderId, actualResponse[i].TeamLeaderId, t)
+				requre.Equal(testTeam.Members, actualResponse[i].Members, t)
+			}
 		})
 	}
 }
 
 func TestTeamRepository_GetTeamByToken(t *testing.T) {
 	db := MustOpenDB(t)
+	defer MustCloseDB(t, db)
+
 	repo := postgres.NewTeamRepository(db)
 	requre := require.New(t)
 
 	type args struct {
-		token string
+		token        string
+		username     string
+		password     string
+		contact      string
+		teamName     string
+		teamLeaderId int
 	}
 	testTable := []struct {
 		name         string
@@ -175,49 +243,66 @@ func TestTeamRepository_GetTeamByToken(t *testing.T) {
 		{
 			name: "OK_1",
 			args: args{
-				token: "e01e949e-9dd0-428e-96c0-28adebf4df3d",
-			},
-			wantResponse: &domain.Team{
-				Id:           2,
-				Name:         "Team2",
-				TeamLeaderId: 2,
-				Token:        "e01e949e-9dd0-428e-96c0-28adebf4df3d",
-			},
-			wantErr: nil,
-		},
-		{
-			name: "OK_2",
-			args: args{
-				token: "7c8b4c73-9fdd-4fbb-b926-43de9aa6f24d",
+				username:     "User1",
+				password:     "User1Pswd",
+				contact:      "@contact",
+				teamName:     "Team1",
+				teamLeaderId: 1,
 			},
 			wantResponse: &domain.Team{
 				Id:           1,
 				Name:         "Team1",
 				TeamLeaderId: 1,
-				Token:        "7c8b4c73-9fdd-4fbb-b926-43de9aa6f24d",
+				Token:        "",
 			},
 			wantErr: nil,
 		},
 	}
 	for _, testCase := range testTable {
 		t.Run(testCase.name, func(t *testing.T) {
-			actualResponse, actualErr := repo.GetTeamByToken(testCase.args.token)
+			user := &domain.User{
+				Username: testCase.args.username,
+				Password: testCase.args.password,
+				Contact:  testCase.args.contact,
+			}
+			helpCreateUser(t, db, user)
 
+			team := &domain.Team{
+				Name:         testCase.args.teamName,
+				TeamLeaderId: testCase.args.teamLeaderId,
+			}
+			helpCreateTeam(t, db, team)
+
+			team = helpGetTeamById(t, db, testCase.wantResponse.Id)
+			testCase.args.token = team.Token
+			testCase.wantResponse.Token = team.Token
+
+			actualResponse, actualErr := repo.GetTeamByToken(testCase.args.token)
 			requre.Equal(testCase.wantErr, actualErr, t)
 			requre.Equal(testCase.wantResponse, actualResponse, t)
 		})
 	}
 }
 
+//Add: team tessting error response!
 func TestTeamRepository_AddMember(t *testing.T) {
 	db := MustOpenDB(t)
-	userRepo := postgres.NewUserRepository(db)
+	defer MustCloseDB(t, db)
+
 	teamRepo := postgres.NewTeamRepository(db)
 	requre := require.New(t)
 
 	type args struct {
-		teamId int
-		userId int
+		teamId       int
+		userId       int
+		username1    string
+		username2    string
+		password1    string
+		password2    string
+		contact1     string
+		contact2     string
+		teamName     string
+		teamLeaderId int
 	}
 	testTable := []struct {
 		name string
@@ -229,19 +314,82 @@ func TestTeamRepository_AddMember(t *testing.T) {
 		{
 			name: "OK",
 			args: args{
-				teamId: 1,
-				userId: 4,
+				teamId:       1,
+				userId:       2,
+				username1:    "User1",
+				username2:    "User2",
+				password1:    "User1Pswd",
+				password2:    "User2Pswd",
+				contact1:     "@contact",
+				contact2:     "@contact2",
+				teamName:     "Team1",
+				teamLeaderId: 1,
 			},
 			wantErr: nil,
 		},
 	}
 	for _, testCase := range testTable {
 		t.Run(testCase.name, func(t *testing.T) {
-			_, err := userRepo.SaveUser("TestUser", "TestPswd", "@testcontact")
-			requre.Equal(testCase.wantErr, err, t)
+			user1 := &domain.User{
+				Username: testCase.args.username1,
+				Password: testCase.args.password1,
+				Contact:  testCase.args.contact1,
+			}
+			helpCreateUser(t, db, user1)
+
+			user2 := &domain.User{
+				Username: testCase.args.username2,
+				Password: testCase.args.password2,
+				Contact:  testCase.args.contact2,
+			}
+			helpCreateUser(t, db, user2)
+
+			team := &domain.Team{
+				Name:         testCase.args.teamName,
+				TeamLeaderId: testCase.args.teamLeaderId,
+			}
+			helpCreateTeam(t, db, team)
+
+			helpSetSettings(t, db, "team_limit", "4")
+			helpSetSettings(t, db, "value", "team_limit")
 
 			actualErr := teamRepo.AddMember(testCase.args.teamId, testCase.args.userId)
 			requre.Equal(testCase.wantErr, actualErr, t)
 		})
 	}
+}
+
+func helpCreateTeam(t *testing.T, db *sqlx.DB, team *domain.Team) *domain.Team {
+	t.Helper()
+
+	id, err := postgres.NewTeamRepository(db).SaveTeam(team.TeamLeaderId, team.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	team.Id = id
+
+	return team
+}
+
+func helpGetTeamById(t *testing.T, db *sqlx.DB, id int) *domain.Team {
+	t.Helper()
+
+	team, err := postgres.NewTeamRepository(db).GetTeamById(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return team
+}
+
+func helpSetSettings(t *testing.T, db *sqlx.DB, key string, value string) int {
+	t.Helper()
+
+	id, err := postgres.NewSettingsRepository(db).Set(key, value)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return id
 }
