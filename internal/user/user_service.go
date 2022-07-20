edd104e8e1a19136ed07c84536cbc71d3bffb3f6
rmnvlv <romario2000@bk.ru>
2022-07-20 23:39:20 +0300

    add transaction

--- internal/user/user_service.go
+++ internal/user/user_service.go
@@ -2,20 +2,24 @@ package user
 
 import (
 	"borda/internal/pkg/core"
+	"borda/pkg/transaction"
+	"context"
 	"errors"
 )
 
 type UserService struct {
 	userRepository core.UserRepository
 	teamRepository core.TeamRepository
+	txManager      transaction.Manager
 }
 
 func NewUserService(userRepository core.UserRepository,
-	teamRepository core.TeamRepository) *UserService {
+	teamRepository core.TeamRepository, txManager transaction.Manager) *UserService {
 
 	return &UserService{
 		userRepository: userRepository,
 		teamRepository: teamRepository,
+		txManager:      txManager,
 	}
 }
 
@@ -29,7 +33,23 @@ type UserResponse struct {
 }
 
 func (us *UserService) GetUser(id int) (UserResponse, error) {
-	user, err := us.userRepository.FindById(id)
+	var actualUser User
+
+	err := us.txManager.Run(
+		context.Background(),
+		func(ctx context.Context) error {
+			user, err := us.userRepository.FindById(ctx, id)
+			actualUser = user
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+		true,
+	)
+
 	if err != nil {
 		return UserResponse{}, err
 	}
@@ -37,15 +57,31 @@ func (us *UserService) GetUser(id int) (UserResponse, error) {
 	// team, err := us.teamRepository.FindById(user.TeamId)
 
 	response := UserResponse{
-		Id:       user.Id,
-		Username: user.Username,
+		Id:       actualUser.Id,
+		Username: actualUser.Username,
 	}
 
 	return response, nil
 }
 
 func (us *UserService) GetAllUsers() ([]UserResponse, error) {
-	users, err := us.userRepository.FindAll()
+	var actualUsers []User
+
+	err := us.txManager.Run(
+		context.Background(),
+		func(ctx context.Context) error {
+			users, err := us.userRepository.FindAll(ctx)
+			actualUsers = users
+			if err != nil {
+				// Wrap error!!!!
+				return err
+			}
+
+			return nil
+		},
+		true,
+	)
+
 	if err != nil {
 		// Wrap error!!!!
 		return []UserResponse{}, err
@@ -53,7 +89,7 @@ func (us *UserService) GetAllUsers() ([]UserResponse, error) {
 
 	response := make([]UserResponse, 0)
 
-	for _, user := range users {
+	for _, user := range actualUsers {
 
 		// TODO
 		// team, err := us.teamRepository.FindById(user.TeamId)
